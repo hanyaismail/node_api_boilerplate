3619aaf186277c41119dbf86c3cd4ab8f80b4b2e
Ismail <ismail.kerja95@gmail.com>
2018-01-18 19:23:32 +0700

    make post request

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,13 @@
-module.exports = function(app, db){
+module.exports = function(app, db1){
 	app.post('/notes', (req, res) => {
-		// We'll create the note here
-		res.send('Hello');
+		const note = {text: req.body.body, title: req.body.title};
+		const database = db1.db('notable');
+		database.collection('notes').insert(note, (err, result) => {
+			if (err) {
+				res.send({'error': 'An error has occured'});
+			} else {
+				res.send(result.ops[0]);
+			}
+		})
 	})
 }
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-
+const db = require('./config/db');
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 const port = 8000;
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
+MongoClient.connect(db.url, (err, database) => {
+	if (err) return console.log(err);
+	require('./app/routes')(app, database);
+	app.listen(port, () => {
 	console.log('Live server on port' + port);
-});
+	});
+})
